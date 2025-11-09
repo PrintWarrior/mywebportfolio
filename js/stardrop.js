c947f999eb9768bfa79d4476725e44dacf7bb32a
CopyPasteWarrior <xavieraceclark.azcona@nmsc.edu.ph>
2025-11-09 23:24:51 +0800

    index 0, commit

---
+++ js/stardrop.js
@@ -0,0 +1,13 @@
+setInterval(()=>{
+    const p=document.createElement("div");
+    p.innerText="✦";
+    p.style.position="fixed";
+    p.style.left=Math.random()*innerWidth+"px";
+    p.style.top="-20px";
+    p.style.opacity=".6";
+    p.style.fontSize="14px";
+    p.style.transition="2s linear";
+    document.body.appendChild(p);
+    setTimeout(()=>p.style.top=innerHeight+"px",50);
+    setTimeout(()=>p.remove(),2000);
+},300);
